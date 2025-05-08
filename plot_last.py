eb9d6a1cb0fbb9a2e599e0a648feb3b996f50065
Javier Gatón Herguedas <gaton@goa.uva.es>
2025-05-08 16:47:34 +0200

    added plot script

---
+++ plot_last.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+from pyrano.application import graphs
+
+def main():
+    station = sys.argv[-1]
+    path = f"plots/last_radiation_{station}.jpg"
+    graphs.plot_last_hours(25, station, path)
+
+if __name__ == "__main__":
+    main()
