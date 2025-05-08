f6159f7786035ca28b6a89f9921d0de47c652bcf
Javier Gatón Herguedas <gaton@goa.uva.es>
2025-05-08 16:16:13 +0200

    read csv only reading .data rows

--- pyrano/data_access/raw.py
+++ pyrano/data_access/raw.py
@@ -1,6 +1,7 @@
 """Read data from pyranometer ouput files"""
 import os
 from datetime import datetime, timedelta
+from io import StringIO
 
 import pandas as pd
 
@@ -9,7 +10,10 @@ from pyrano.common.models import StorageSettings
 _BASECOLS = ["date", "time", "radiation", "temp", "voltage"]
 
 def read_file(path: str) -> pd.DataFrame:
-    df = pd.read_csv(path, sep=';', skiprows=1, usecols=[1,2,3,4,5], names=_BASECOLS)
+    with open(path, encoding="utf-8") as f:
+        lines = [line for line in f if line.startswith(".data")]
+    csvdata = StringIO("\n".join(lines))
+    df = pd.read_csv(csvdata, sep=';', usecols=[1,2,3,4,5], names=_BASECOLS)
     df['measured_at'] = pd.to_datetime(df['date'] + ' ' + df['time'])
     df = df.drop(columns=['date', 'time'])
     return df
