ef3cf27ba06697e84723e45db61922b0e0a06e19
Javier Gatón Herguedas <gaton@goa.uva.es>
2025-05-08 16:51:52 +0200

    last_hours in send client can be set cli

--- send_client.py
+++ send_client.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+import sys
+
 from client.post_measurements import read_post
 
 def main():
-    read_post(2, config_path = 'config.yml')
+    last_hours = float(sys.argv[-1])
+    read_post(last_hours, config_path = 'config.yml')
 
 if __name__ == "__main__":
     main()
