da895547aacce418b7a25590771921ede4c72e5a
Javier Gatón Herguedas <gaton@goa.uva.es>
2025-05-08 16:44:38 +0200

    added utc to date info

--- pyrano/business/plots.py
+++ pyrano/business/plots.py
@@ -28,5 +28,5 @@ def _finish_plot(axes: List[Axes], path: str):
 def plot_measurements(df: pd.DataFrame, path: str):
     ax = df.plot.scatter('measured_at', 'value', 8, "orange", figsize=_FIGSIZE)
     ax.set_ylabel("Radiation (W/m²)")
-    ax.set_xlabel("Date")
+    ax.set_xlabel("Date (UTC)")
     _finish_plot([ax], path)
