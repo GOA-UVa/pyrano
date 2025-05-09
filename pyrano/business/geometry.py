480e9ece6b3f0e82928013b01866e9dfe260e848
Javier Gatón Herguedas <gaton@goa.uva.es>
2025-05-09 15:14:29 +0200

    trying to crrect solar day mask

--- pyrano/business/geometry.py
+++ pyrano/business/geometry.py
@@ -4,5 +4,5 @@ from pvlib import solarposition
 
 def filter_solar_day(df, lat, lon) -> pd.DataFrame:
     solpos = solarposition.get_solarposition(df["measured_at"], lat, lon)
-    df = df[solpos['apparent_elevation'] > 0]
+    df = df[solpos['apparent_elevation'].values > 0]
     return df
