b55e9627a1fbf261aabcf79ff3308478026cecf0
Javier Gatón Herguedas <gaton@goa.uva.es>
2025-05-09 17:31:26 +0200

    passing datetime not df

--- pyrano/application/graphs.py
+++ pyrano/application/graphs.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from pyrano.business import data, plots, geometry
 from pyrano.common.config import get_server_config
 from pyrano.common.constants import SERVER_CONFIG_DEFAULT_PATH
@@ -13,5 +15,5 @@ def plot_today(station: str, plotpath: str, config_path = SERVER_CONFIG_DEFAULT_
     df = data.get_utctoday_measurements(station, cfg)
     lat = dfstat['latitude'].values[0]
     lon = dfstat['longitude'].values[0]
-    dt0, dtf = geometry.get_solar_day_lims(df, lat, lon)
+    dt0, dtf = geometry.get_solar_day_lims(datetime.now(), lat, lon)
     plots.plot_measurements(df, plotpath, (dt0, dtf))
