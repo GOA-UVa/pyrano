735e4a36aea1a5a08402cf6b18bf4af92f8862a5
Javier Gatón Herguedas <gaton@goa.uva.es>
2026-05-08 13:17:44 +0200

    secured filtering of invalid lines

--- pyrano/data_access/raw.py
+++ pyrano/data_access/raw.py
@@ -2,6 +2,7 @@
 import os
 from datetime import datetime, timedelta
 from io import StringIO
+from typing import Union
 
 import pandas as pd
 
@@ -9,10 +10,12 @@ from pyrano.common.models import StorageSettings
 
 _BASECOLS = ["date", "time", "radiation", "temp", "voltage"]
 
-def read_file(path: str) -> pd.DataFrame:
+def read_file(path: str) -> Union[pd.DataFrame, None]:
     with open(path, encoding="utf-8") as f:
         lines = [line for line in f if line.startswith(".data")]
     lines = [line for line in lines if line.count(';') > 5]
+    if not lines:
+        return None
     csvdata = StringIO("\n".join(lines))
     df = pd.read_csv(csvdata, sep=';', usecols=[1,2,3,4,5], names=_BASECOLS)
     df['measured_at'] = pd.to_datetime(df['date'] + ' ' + df['time'])
@@ -28,6 +31,7 @@ def read_data_between(first: datetime, last: datetime, conf: StorageSettings) ->
     dfs = []
     for f in files:
         dfs.append(read_file(os.path.join(conf.output_dir, f)))
+    dfs = [df for df in dfs if df is not None and not df.empty]
     df = pd.concat(dfs)
     df = df[(df['measured_at'] >= first) & (df['measured_at'] <= last)]
     df = df.drop(columns=["voltage"])
